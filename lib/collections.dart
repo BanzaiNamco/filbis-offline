440a25be23392fe6068bdb9eb09049e8c85f0d41
BanzaiNamco <orrin_landon_uy@dlsu.edu.ph>
2024-06-20 16:25:38 +0800

    (inc) connect to python server and download all modules

--- lib/collections.dart
+++ lib/collections.dart
@@ -1,31 +1,32 @@
 import 'package:isar/isar.dart';
 
-@Collection()
-class module {
-  int id = Isar.autoIncrement;
-  String name = "";
-  List<sub_module>? sub_modules;
+part 'collections.g.dart';
+
+@collection
+class Module {
+  Id id = Isar.autoIncrement;
+  late String name;
+  List<sub_module>? subModule;
 }
 
-@Collection()
+@embedded
 class sub_module {
-  int id = Isar.autoIncrement;
+  // final qckReply = IsarLink<qck_reply>();
   qck_reply? qckReply;
   question_translation? questionTranslation;
-  String name = "";
+  late String name;
 }
 
-@Collection()
+@embedded
 class qck_reply {
-  int id = Isar.autoIncrement;
+  // int id = Isar.autoIncrement;
   List<String>? cebuano_replies;
   List<String>? english_replies;
   List<String>? tagalog_replies;
 }
 
-@Collection()
+@embedded
 class question_translation {
-  int id = Isar.autoIncrement;
   String? cebuano_response;
   String? english_response;
   String? tagalog_response;
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,11 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'dart:async' show Future;
 import 'package:flutter/services.dart' show rootBundle;
+import 'package:path_provider/path_provider.dart';
+import 'package:http/http.dart' as http;
+import 'package:isar/isar.dart';
+
+import 'collections.dart';
 
 void main() {
   runApp(const MainApp());
@@ -11,14 +16,21 @@ class MainApp extends StatefulWidget {
   const MainApp({Key? key}) : super(key: key);
 
   @override
-  State<MainApp> createState() => _MainAppState();
-
-  
+  State<MainApp> createState() => _MainAppState();  
 }
 
 class _MainAppState extends State<MainApp> {
   var _loadedData = {};
   var answers = {};
+  late Isar isar;
+
+  Future<Isar> initIsar() async {
+    final dir = await getApplicationDocumentsDirectory();
+    return await Isar.open(
+      [ModuleSchema],
+      directory: dir.path
+    );
+  }
 
   Future<void> readJson() async {
     final String response = await rootBundle.loadString('assets/files/sample.json');
@@ -26,12 +38,18 @@ class _MainAppState extends State<MainApp> {
     setState(() {
       _loadedData = data["q1"];
     });
-  }
+}
 
   @override
   void initState() {
     super.initState();
     readJson();
+    initIsar().then((value) => isar = value);
+    // check if the data is already in the database
+    // final count = await isar.modules.count();
+    // if (count == 0){
+      
+    // }
   }
 
   @override
@@ -58,6 +76,26 @@ class _MainAppState extends State<MainApp> {
               IconButton(
               onPressed: () {
                 print("clicked");
+                try {
+                  var url = Uri.http('10.0.2.2:8000', '/mobile_download_modules');
+                  print("workking");
+                  http.post(url, body: {}).then((response) async {
+                    var data = json.decode(response.body);
+                    // for each module in the data, add it to the database
+                    for (var module in data) {
+                      final mod = Module()..name = module.key;
+                      await isar.writeTxn(() async {
+                        await isar.modules.put(mod);
+                      });
+                      final subModules = module.value;
+                      for (var subModule in subModules){
+                        // for each submodule, set the qckreply and stuff
+                      }
+                    }
+                  });
+                } catch (e) {
+                  print(e);
+                }
               },
               icon: Icon(Icons.add),
               ),
